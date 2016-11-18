c0dbe98841664317c6d67ed0fc58a446849a42e0
john.liu <john.liu@cdiniteractive.com>
2016-11-17 22:56:40 -0500

    add controller code

--- client/app/races/races.component.js
+++ client/app/races/races.component.js
@@ -6,10 +6,39 @@ const uiRouter = require('angular-ui-router');
 import routes from './races.routes';
 
 export class RacesComponent {
+  Races = [];
+  newRace = '';
   /*@ngInject*/
   constructor() {
-    this.message = 'Hello';
+      this.$http = $http;
+      this.socket = socket;
+
+      $scope.$on('$destroy', function () {
+          socket.unsyncUpdates('race');
+      });
   }
+
+    $onInit() {
+        this.$http.get('/api/races')
+            .then(response => {
+                this.Races = response.data;
+                this.socket.syncUpdates('race', this.Races);
+            });
+    }
+
+    addThing() {
+        if (this.newRace) {
+            this.$http.post('/api/races', {
+                name: this.newRace
+            });
+            this.newRace = '';
+        }
+    deleteThing(race)
+    {
+        this.$http.delete(`/api/races/${race._id}`);
+    }
+    }
+
 }
 
 export default angular.module('races', [uiRouter])
